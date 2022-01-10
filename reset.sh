9115c3e64e5743f7976674bb522943bef8491fa7
Sam Gardner <stgardner4@tamu.edu>
2022-01-10 16:02:52 -0600

    add reset script

---
+++ reset.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# HDWX operational submodule resetter script
+# Created 10 Janurary 2022 by Sam Gardner <stgardner4@tamu.edu>
+
+rm -rf output/
+rm -rf radarData/
+rm status.txt
+rm plotcmds.txt
