b3e04a25b1c9ba6451c576b5f34eff65219feaac
Sam Gardner <stgardner4@tamu.edu>
2022-02-09 18:50:39 -0600

    add support for condaEnvName

--- generate.sh
+++ generate.sh
@@ -7,18 +7,24 @@ then
   exit
 fi
 touch status.txt
+if [ -f ../config.txt ]
+then
+    source ../config.txt
+else
+    condaEnvName="HDWX"
+fi
 if [ -f plotcmds.txt ]
 then
     rm plotcmds.txt
 fi
 export PYART_QUIET=True
-if [ -f ~/mambaforge/envs/HDWX/bin/python3 ]
+if [ -f ~/mambaforge/envs/$condaEnvName/bin/python3 ]
 then
-    ~/mambaforge/envs/HDWX/bin/python3 adradFetch.py
+    ~/mambaforge/envs/$condaEnvName/bin/python3 adradFetch.py
 fi
-if [ -f ~/miniconda3/envs/HDWX/bin/python3 ]
+if [ -f ~/miniconda3/envs/$condaEnvName/bin/python3 ]
 then
-    ~/miniconda3/envs/HDWX/bin/python3 adradFetch.py
+    ~/miniconda3/envs/$condaEnvName/bin/python3 adradFetch.py
 fi
 plotcmdStr=`cat plotcmds.txt`
 IFS=$'\n' plotcmdArr=($plotcmdStr)
@@ -27,11 +33,11 @@ do
     eval "$plotcmd"
 done
 
-if [ -f ~/mambaforge/envs/HDWX/bin/python3 ]
+if [ -f ~/mambaforge/envs/$condaEnvName/bin/python3 ]
 then
-    ~/mambaforge/envs/HDWX/bin/python3 cleanup.py
+    ~/mambaforge/envs/$condaEnvName/bin/python3 cleanup.py
 fi
-if [ -f ~/miniconda3/envs/HDWX/bin/python3 ]
+if [ -f ~/miniconda3/envs/$condaEnvName/bin/python3 ]
 then
-    ~/miniconda3/envs/HDWX/bin/python3 cleanup.py
+    ~/miniconda3/envs/$condaEnvName/bin/python3 cleanup.py
 fi
